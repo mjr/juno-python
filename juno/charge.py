2d4cfba3acea4bbb7950accf04d34f00d08c935d
Manaia Junior <manaiajr.23@gmail.com>
2021-02-14 09:57:08 -0300

    Add params to filter list charge

--- juno/charge.py
+++ juno/charge.py
@@ -6,8 +6,14 @@ def create(dictionary):
     return handler_request.post(charge_routes.get_base_url(), dictionary)
 
 
-def find_all():
-    return handler_request.get(charge_routes.get_base_url())
+def find_all(start_date=None, end_date=None, size=10):
+    url = f"{charge_routes.get_base_url()}/?pageSize={size}"
+
+    url_params = ""
+    if start_date and end_date:
+        url_params = f"&createdOnStart={start_date}&createdOnEnd={end_date}"
+
+    return handler_request.get(f"{url}{url_params}")
 
 
 def find_by_id(charge_id):
